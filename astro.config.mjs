7015f57f9e85d2af49ed8cb79cc5888017d4de9b
EmFor2001 <k.hajduk.kh@gmail.com>
2022-12-29 13:13:16 +0100

    netlify added v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/",
   integrations: [
